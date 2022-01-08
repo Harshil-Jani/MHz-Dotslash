c75069ddd6de87d207f51700295b35300637128e
Harshil Jani <harshiljani2002@gmail.com>
2022-01-09 00:11:43 +0530

    Options Updated

--- options.js
+++ options.js
@@ -1,18 +1,20 @@
-function search_board(){
-    let input=document.getElementById('seachbar').value
-    input=input.toLowercase();
-    let x =document.getElementsByClassName('card')
-  
-    for(i=0; i<x.length;i++)
-    {
-      if (!x[i].innerHTML.toLowerCase().includes(input)) {
-        x[i].style.display="none";
-      }
-      
-        else {
-          x[i].style.display="list-item";  
+const before = document.querySelectorAll('.close');
+const after = document.querySelectorAll('.open');
+const icon = document.getElementsByClassName('.icon')
 
-       }
-  
-    }
-  }
+for (let i = 0; i < before.length; i++) {
+    before[i].addEventListener('click', () => {
+        if (after[i].classList.contains('open')) {
+            after[i].classList.add('close');
+            before[i].classList.remove('open');
+        }
+    });
+
+    icon.addEventListener('click', () => {
+        if (before[i].classList.contains('close')) {
+            before[i].classList.add('open');
+            after[i].classList.remove('open');
+        }
+    });
+
+}
